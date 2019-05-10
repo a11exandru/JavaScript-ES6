50a4638a4add00651a90bc648ef8c183717a90b0
a11exandru <alexpuiu20@yahoo.com>
2019-05-10 21:35:33 +0300

    adaugare sageata catre top && schimbare stil header

--- app.js
+++ app.js
@@ -13,11 +13,27 @@ function closeMenu() {
 // Acest cod face sa apara definitia cand scrollam 
 
 window.addEventListener("scroll", () => {
+	let header = document.querySelector('#header');
 	let text = document.getElementById('animatie-definitie');
 	let gif = document.getElementById('gif');
-	text.classList.add('text-animation');
+	let topArrow = document.getElementById('top-arrow');
 	gif.classList.add('gif-animation');
+	text.classList.add('text-animation');
 
-	// animatie text #istorie
-	document.getElementById("schema").style.color = "white";
+	if(window.scrollY < 150) {
+		header.style.backgroundColor = '#292E49';
+	} else {
+		header.style.backgroundColor = "#111";
+	}
+
+	if(window.scrollY < 260) {
+		topArrow.style.opacity = '0';
+	} else {
+		topArrow.style.opacity = '1';
+	}
+
+	if(window.scrollY > 490) {
+		// animatie text #istorie
+		document.getElementById("schema").style.color = "white";
+	}
 });
