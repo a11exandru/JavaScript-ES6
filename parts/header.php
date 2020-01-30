1164dd8f851e02b47d71fa00728c9144b8e7b187
a11exandru <alexpuiu20@yahoo.com>
2020-01-30 17:18:39 +0200

    Changes according to GitHub terms&conditions.

--- parts/header.php
+++
@@ -1,4 +0,0 @@
-<header id = "header">
-    <span onclick="showMenu()" class="fa fa-bars showMenu"></span>
-    <div class="titlu-header">C++</div>
-</header>
--- parts/menuSay.php
+++
@@ -1,6 +0,0 @@
-<div class = "menu" id = "menu">
-    <div class="menu-links">
-        <a href="javascript:void(0)" onclick="closeMenu()" class="closeMenu">&times;</a>
-        <a href="/", onclick="closeMenu()">Pagina de pornire</a>
-    </div>
-</div>
